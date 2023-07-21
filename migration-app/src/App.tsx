818c4e400b33898ee3de50f9d87ad3d0f9745277
a464442700 <464442700@qq.com>
2023-07-21 17:53:08 +0800

    2023-07-21

--- migration-app/src/App.tsx
+++ migration-app/src/App.tsx
@@ -10,6 +10,8 @@ import {
 import type {MenuProps} from 'antd';
 import {Breadcrumb, Layout, Menu, theme} from 'antd';
 
+import AllDependency from './component/AllDependency';
+
 const {Header, Content, Footer, Sider} = Layout;
 
 type MenuItem = Required<MenuProps>['items'][number];
@@ -17,57 +19,72 @@ type MenuItem = Required<MenuProps>['items'][number];
 function getItem(
     label: React.ReactNode,
     key: React.Key,
-    labalCode:string,
     icon?: React.ReactNode,
     children?: MenuItem[],
-
 ): MenuItem {
     return {
         key,
         icon,
-        labalCode,
+
         children,
         label,
 
     } as MenuItem;
 }
-function selectMenu(menuItem:MenuItem| null){
-    if (menuItem?.key) {
-        console.log(menuItem.key);
-    }
-}
+
+
 
 const items: MenuItem[] = [
 
-    getItem('文件下载', 1,'downloadFile', <FileOutlined/>, [
-        getItem('源码下载', 2,'oneSource'),
-        getItem('对比代码下载', 3,'compareSource')
+    getItem('文件下载', 1, <FileOutlined/>, [
+        getItem('源码下载', 2),
+        getItem('对比代码下载', 3)
 
     ]),
 
 ];
-
 const App: React.FC = () => {
-    const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(false);//是否展开
+    const [currentPageKey, setCurrentPageKey] = useState<number>(1);
+    const [renderComponent,setRenderComponent]=useState();
     const {
         token: {colorBgContainer},
     } = theme.useToken();
+
+    const selectMenu = (menuItem: MenuItem | null) => {
+        if (menuItem?.key) {
+            // @ts-ignore
+            setCurrentPageKey(menuItem.key)
+           // @ts-ignore
+            if (menuItem.key==2){
+               // @ts-ignore
+               setRenderComponent(<AllDependency/>)
+           }else if (menuItem.key==3)
+            { // @ts-ignore
+                setRenderComponent(<div>3</div>)}
+        }
+    };
+
+
 
     return (
         <Layout style={{minHeight: '100vh'}}>
             <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
                 <div className="demo-logo-vertical"/>
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={selectMenu}/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}
+                      onClick={selectMenu}/>
             </Sider>
             <Layout>
                 <Header style={{padding: 0, background: colorBgContainer}}/>
                 <Content style={{margin: '0 16px'}}>
                     <Breadcrumb style={{margin: '16px 0'}}>
-                        {/*<Breadcrumb.Item>User</Breadcrumb.Item>*/}
-                        {/*<Breadcrumb.Item>Bill</Breadcrumb.Item>*/}
+
                     </Breadcrumb>
                     <div style={{padding: 24, minHeight: 360, background: colorBgContainer}}>
-                        Bill is a cat.
+
+                        {renderComponent}
+
+
                     </div>
                 </Content>
                 <Footer style={{textAlign: 'center'}}>Ant Design ©2023 Created by liuxiaofeng </Footer>
---
+++ migration-app/src/component/AllDependency.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Button, Form, Input, Select } from 'antd';
+import type { FormInstance } from 'antd/es/form';
+
+const { Option } = Select;
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
+//测试数据
+const dataSources=[{"name":"dupdb","value":"local"},{"name":"xepdb1","value":"remote"}];
+
+const AllDependency: React.FC = () => {
+    const formRef = React.useRef<FormInstance>(null);
+
+    const onGenderChange = (value: string) => {
+        switch (value) {
+            case 'male':
+                formRef.current?.setFieldsValue({ note: 'Hi, man!' });
+                break;
+            case 'female':
+                formRef.current?.setFieldsValue({ note: 'Hi, lady!' });
+                break;
+            case 'other':
+                formRef.current?.setFieldsValue({ note: 'Hi there!' });
+                break;
+            default:
+                break;
+        }
+    };
+
+    const onFinish = (values: any) => {
+        console.log(values);
+    };
+
+    const onReset = () => {
+        formRef.current?.resetFields();
+    };
+
+    const onFill = () => {
+        formRef.current?.setFieldsValue({ note: 'Hello world!', gender: 'male' });
+    };
+
+    return (
+        <Form
+            {...layout}
+            ref={formRef}
+            name="control-ref"
+            onFinish={onFinish}
+            style={{ maxWidth: 600 }}
+        >
+            <Form.Item name="note" label="数据源" rules={[{required: true}]}>
+
+                <Select
+                    placeholder="配置文件对应的数据源"
+                    onChange={onGenderChange}
+                    allowClear>
+                    <Option value="local">local</Option>
+                    <Option value="remote">remote</Option>
+
+                </Select>
+
+            </Form.Item>
+
+
+            <Form.Item name="note" label="数据源" rules={[{ required: true }]}>
+
+               <Input/>
+            </Form.Item>
+
+
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+                <Button htmlType="button" onClick={onReset}>
+                    Reset
+                </Button>
+                <Button type="link" htmlType="button" onClick={onFill}>
+                    Fill form
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default AllDependency;
